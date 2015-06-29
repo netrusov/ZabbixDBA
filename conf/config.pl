e52ed50621eb05ed16be82e3b1af9bec5ac9199b
anetrusov <al.netrusov@gmail.com>
2015-06-29 18:21:04 +0300

    Update config.pl

--- conf/config.pl
+++ conf/config.pl
@@ -1,8 +1,8 @@
 {
     zabbix_server_list => ['zabbix01'],
-    smoszabbix01       => {
+    zabbix01           => {
         zabbix01 => '192.168.1.100',
-        port    => 10051
+        port     => 10051
     },
     daemon => { sleep => 120 },
     database_list => [ 'XXXPRD', 'XXXDEV' ],
@@ -12,7 +12,7 @@
         query_list_file => './conf/query.props.pl'
     },
     XXXPRD => { dsn => 'DBI:Oracle:host=xxxdb11;port=1521;sid=XXXPRD', },
-    XXXDEV  => {
+    XXXDEV => {
         dsn  => 'DBI:Oracle:host=xxxdb01;port=1521;sid=XXXDEV',
         user => 'c##zabbix'
     }

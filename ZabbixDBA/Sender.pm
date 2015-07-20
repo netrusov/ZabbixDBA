bb17f190f20c5c461b7e22907297afda940f978b
Alexander Netrusov <Alexander.Netrusov@in-touch.ru>
2015-07-20 14:45:15 +0300

    checking if data in array is an array reference

--- ZabbixDBA/Sender.pm
+++ ZabbixDBA/Sender.pm
@@ -11,13 +11,17 @@ our $VERSION = '1.010';
 use IO::Socket::INET;
 use MIME::Base64 qw(encode_base64);
 
-my $data_template = <<'EOF';
+my $html_template = <<'EOF';
 <req>
 <host>%s</host>
 <key>%s</key>
 <data>%s</data>
 </req>
 EOF
+
+# May be someday I'll use JSON to send data to Zabbix
+my $json_template
+    = q(ZBXDS{"request":"sender data","data":[{"host":"%s","key":"%s","value":"%s"}]});
 
 sub new {
     my ( $class, %server_list ) = @_;
@@ -26,9 +30,10 @@ sub new {
 }
 
 sub encode {
-    # MIME::Base64::encode_base64() adds newline at the end of string,
-    # but Zabbix doesn't like them, and chomp() doesn't work properly with sprintf.
-    # That's why this subroutine was created
+
+# MIME::Base64::encode_base64() adds newline at the end of string,
+# but Zabbix doesn't like them, and chomp() doesn't work properly with sprintf.
+# That's why this subroutine was created
     chomp( my $result = encode_base64(shift) );
     return $result;
 }
@@ -36,6 +41,7 @@ sub encode {
 sub send {
     my ( $self, @data ) = @_;
     for my $sever ( values %{$self} ) {
+
         for (@data) {
             if ( !ref $_ ) {
                 next;
@@ -48,15 +54,13 @@ sub send {
 
             if ( !$socket ) {
                 confess sprintf
-                    'Unable to connect to server: %s:%d',
+                    'Unable to connect to server %s:%d',
                     $sever->{address},
                     $sever->{port};
             }
             my ( $host, $key, $data ) = @{$_};
-            my $data_base64 = sprintf $data_template,
-                encode($host),
-                encode($key),
-                encode($data);
+            my $data_base64 = sprintf $html_template,
+                encode($host), encode($key), encode($data);
             $socket->send($data_base64);
             $socket->close();
         }

036cb5002b47265cc2f2a3cb82219b75e46b5c80
Alexander Netrusov <Alexander.Netrusov@in-touch.ru>
2015-08-26 14:19:03 +0300

    small fixes

--- settings.sh
+++ settings.sh
@@ -3,6 +3,7 @@ LD_LIBRARY_PATH=/usr/share/oracle/instantclient_12_1
 TNS_ADMIN=$ORACLE_HOME
 PATH=$ORACLE_HOME:$PATH
 
+PERL_BIN=/usr/local/bin/perl
 CONFIG=/opt/ZabbixDBA/conf/config.pl
 LOCKFILE=/var/lock/subsys/ZabbixDBA
 PIDFILE=/var/run/ZabbixDBA.pid

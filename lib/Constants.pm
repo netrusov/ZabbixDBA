adcca414f0e824bdc00ab83f0ceef7de703a8bf1
Alexander Netrusov <Alexander.Netrusov@in-touch.ru>
2015-09-17 15:33:56 +0300

    fix wrong syntax

--- lib/Constants.pm
+++ lib/Constants.pm
@@ -6,19 +6,19 @@ use strict;
 use warnings FATAL => 'all';
 
 use Readonly;
-use Exporter;
+require Exporter;
 
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(
-*VERSION
-*PROJECT_NAME
-*RETRY_COUNT
-*SLEEP
+    *VERSION
+    *PROJECT_NAME
+    *RETRY_COUNT
+    *SLEEP
 );
 
-Readonly our $VERSION = '2.000';
-Readonly our $PROJECT_NAME = 'ZabbixDBA';
-Readonly our $RETRY_COUNT = 2;
-Readonly our $SLEEP => 120;
+Readonly our $VERSION      => '2.000';
+Readonly our $PROJECT_NAME => 'ZabbixDBA';
+Readonly our $RETRY_COUNT  => 2;
+Readonly our $SLEEP        => 120;
 
 1;

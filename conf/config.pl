d969b6480c257610946ac1f6d3da7837d08eb9d1
Alexander Netrusov <Alexander.Netrusov@gmail.com>
2016-04-01 18:45:16 +0300

    changed controlfile structure

--- conf/config.pl
+++ conf/config.pl
@@ -1,26 +1,33 @@
-{   
-    zabbix_server_list => ['zabbix01'],
-    zabbix01           => {
-        address => '192.168.1.100',
-        port    => 10051
+{
+    zabbix => {
+        server => '192.168.1.100',
+        port   => 10051,
+        timeout => 30,
     },
-    daemon => { sleep => 120, maxproc => 20, },
-    database_list => [ 'XXXPRD', 'XXXDEV' ],
-    default       => {
-        user            => 'zabbix',
-        password        => 'zabbix',
-        query_list_file => '/opt/ZabbixDBA/conf/query.props.pl'
+    daemon => {
+        sleep       => 120,
+        maxproc     => 20,
+        retry_count => 1,
     },
-    XXXPRD => {
-        dsn   => 'DBI:Oracle:host=xxxdb11;port=1521;sid=XXXPRD',
-        sleep => 60
-    },
-    XXXDEV => {
-        dsn  => 'DBI:Oracle:host=xxxdb01;port=1521;sid=XXXDEV',
-        user => 'c##zabbix',
-    },
-    XXXTST => {
-        dsn  => 'DBI:Oracle:host=xxxdb02;port=1521;sid=XXXTST',
-        retry_count => 2
+    db => {
+        default => {
+            user       => 'zabbix',
+            password   => 'zabbix',
+            query_list => 'query.props.pl'
+        },
+        list   => [ 'XXXPRD', 'XXXDEV' ],
+        XXXPRD => {
+            dsn              => 'DBI:Oracle:host=xxxdb11;port=1521;sid=XXXPRD',
+            extra_query_list => 'extra.query.props.pl',
+            sleep            => 60,
+        },
+        XXXDEV => {
+            dsn  => 'DBI:Oracle:host=xxxdb01;port=1521;sid=XXXDEV',
+            user => 'c##zabbix',
+        },
+        XXXTST => {
+            dsn         => 'DBI:Oracle:host=xxxdb02;port=1521;sid=XXXTST',
+            retry_count => 2,
+        }
     }
 }

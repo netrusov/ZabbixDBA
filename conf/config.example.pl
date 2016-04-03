ddc121c6cda3d3da4dfb1516abcdbeb3e9c0482b
Alexander Netrusov <Alexander.Netrusov@gmail.com>
2016-04-03 21:56:03 +0300

    updated config example

--- conf/config.example.pl
+++ conf/config.example.pl
@@ -6,27 +6,27 @@
     },
     daemon => {
         sleep       => 120,
-        maxproc     => 20,
         retry_count => 1,
     },
     db => {
         default => {
             user       => 'zabbix',
             password   => 'zabbix',
-            query_list => 'query.props.pl'
+            query_list => 'query.props.pl',
+            sleep      => 30,
         },
         list   => [ 'XXXPRD', 'XXXDEV' ],
         XXXPRD => {
             dsn              => 'DBI:Oracle:host=xxxdb11;port=1521;sid=XXXPRD',
             extra_query_list => 'extra.query.props.pl',
-            sleep            => 60,
+            sleep            => 15,
         },
         XXXDEV => {
             dsn  => 'DBI:Oracle:host=xxxdb01;port=1521;sid=XXXDEV',
             user => 'c##zabbix',
         },
         XXXTST => {
-            dsn         => 'DBI:Oracle:host=xxxdb02;port=1521;sid=XXXTST',
+            dsn         => 'DBI:Pg:database=xxxtst;host=xxxdb02;port=5432',
             retry_count => 2,
         }
     }
